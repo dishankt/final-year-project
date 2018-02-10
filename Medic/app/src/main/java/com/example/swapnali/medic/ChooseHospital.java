0a26c209eb05aad362f533bacdbda44593af9ffc
Chinmay <ckkadam1999@gmail.com>
2018-02-10 06:07:36 +0530

    Android app

--- Medic/app/src/main/java/com/example/swapnali/medic/ChooseHospital.java
+++ Medic/app/src/main/java/com/example/swapnali/medic/ChooseHospital.java
@@ -2,12 +2,26 @@ package com.example.swapnali.medic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ChooseHospital extends AppCompatActivity {
-
+    ListView listView;
+    ArrayList<JSONObject> jsonObjects;
+    ArrayList<String> hospitalNames;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_hospital);
+        listView = findViewById(R.id.listView);
+        jsonObjects = new ArrayList<>();
+        hospitalNames = new ArrayList<>();
+
     }
 }
